b350f143fdc7c878278f13ca140466814162c6f3
youn <tty7797@naver.com>
2025-01-22 05:30:06 +0900

    fix : @EnableJpaAuditing 추가(DB에서 자동으로 createdTime ,upadateTime 생성하기 위해서)

--- src/main/java/CTHH/chanstagram/ChanstagramApplication.java
+++ src/main/java/CTHH/chanstagram/ChanstagramApplication.java
@@ -2,12 +2,14 @@ package CTHH.chanstagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChanstagramApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChanstagramApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChanstagramApplication.class, args);
+    }
 
 }
