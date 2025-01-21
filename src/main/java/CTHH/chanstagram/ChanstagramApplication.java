ddf1ddaf814e3eb07a8c5709d89d422e846fc5e8
TJOEUNIS <HwaEVE@gmail.com>
2025-01-21 17:27:05 +0900

    기본 설정, 베이스 엔티티, 해쉬화 설정

---
+++ src/main/java/CTHH/chanstagram/ChanstagramApplication.java
@@ -0,0 +1,13 @@
+package CTHH.chanstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CTHH/chanstagram/ChanstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package CTHH.chanstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
