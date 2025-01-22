68ed27f57c2e9afe998cda98a5b74b1fae243f9b
YounTaeWoo <tty7797@naver.com>
2025-01-22 14:56:28 +0900

    feat : 회원 탈퇴 기능 구현

--- src/main/java/CTHH/chanstagram/Comment/CommentRepository.java
+++ src/main/java/CTHH/chanstagram/Comment/CommentRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Modifying
     @Transactional
     @Query("DELETE FROM Comment c WHERE c.post.id = :postId")
     void deleteAllByPostId(@Param("postId") Long postId);
+
+    // loginId로 모든 댓글 삭제
+    void deleteByUser_LoginId(String loginId);
 }
