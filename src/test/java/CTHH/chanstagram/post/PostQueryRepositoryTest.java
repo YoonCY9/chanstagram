be6fb69149dabb5c1396af77049316ab6d983105
hylee <lee@>
2025-01-23 17:21:54 +0900

    feat : user가 좋아요한 post 보여주는 기능 queryDsl test 완료

---
+++ src/test/java/CTHH/chanstagram/post/PostQueryRepositoryTest.java
@@ -0,0 +1,67 @@
+package CTHH.chanstagram.post;
+
+import CTHH.chanstagram.User.Gender;
+import CTHH.chanstagram.User.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class PostQueryRepositoryTest {
+    @Autowired
+    private PostQueryRepository postQueryRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void 유저별로좋아요리스트() {
+        User user = new User("윤태우", "youn", "younId", "11111", Gender.Man,
+                LocalDate.parse("2001-08-08"), "잘부탁드립니다!", "ImageUrl", "01074877796");
+        em.persist(user);
+        Post post1 = new Post("",List.of(),user);
+        Post post2 = new Post("",List.of(),user);
+        Post post3 = new Post("",List.of(),user);
+        Post post4 = new Post("",List.of(),user);
+        Post post5 = new Post("",List.of(),user);
+        em.persist(post1);
+        em.persist(post2);
+        em.persist(post3);
+        em.persist(post4);
+        em.persist(post5);
+
+        User user1 = new User("이호연", "yeon", "yeonId", "11111", Gender.Woman,
+                LocalDate.parse("2001-08-18"), "잘부탁드립니다!!!", "ImageUrl", "11074877796");
+        User user2 = new User("이호", "yeo", "yeoId", "11111", Gender.Woman,
+                LocalDate.parse("2001-08-18"), "잘부탁드립니다!!!", "ImageUrl", "11074877795");
+        User user3 = new User("이", "ye", "yeId", "11111", Gender.Woman,
+                LocalDate.parse("2001-08-18"), "잘부탁드립니다!!!", "ImageUrl", "11074877794");
+
+        em.persist(user1);
+        em.persist(user2);
+        em.persist(user3);
+
+        Like like1 =new Like(user1,post1);
+        Like like2 =new Like(user2,post1);
+        Like like3 =new Like(user3,post1);
+
+        Like like4 =new Like(user1,post2);
+        Like like5 =new Like(user2,post2);
+
+        em.persist(like1);
+        em.persist(like2);
+        em.persist(like3);
+        em.persist(like4);
+        em.persist(like5);
+        System.out.println(postQueryRepository.getLikedPostsByUser(user1.getLoginId()));
+        System.out.println(postQueryRepository.getLikedPostsByUser(user2.getLoginId()));
+        System.out.println(postQueryRepository.getLikedPostsByUser(user3.getLoginId()));
+    }
+}
