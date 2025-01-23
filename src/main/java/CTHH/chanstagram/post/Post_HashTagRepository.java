1cae0c0b483ca11833efd9bfbf3900a319a1fdce
YounTaeWoo <tty7797@naver.com>
2025-01-23 13:29:14 +0900

    feat: Post_HashTagRepository 생성

---
+++ src/main/java/CTHH/chanstagram/post/Post_HashTagRepository.java
@@ -0,0 +1,6 @@
+package CTHH.chanstagram.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Post_HashTagRepository extends JpaRepository<Post_HashTag, Long> {
+}
