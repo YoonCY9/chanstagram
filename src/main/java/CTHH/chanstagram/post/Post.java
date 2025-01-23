f536a42ecac5541d3f752e2dd2bcddb2d671079f
TJOEUNIS <HwaEVE@gmail.com>
2025-01-23 13:09:52 +0900

    feat : follow 하기 구현

--- src/main/java/CTHH/chanstagram/post/Post.java
+++ src/main/java/CTHH/chanstagram/post/Post.java
@@ -13,6 +13,7 @@ public class Post extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String content;
 
     private int commentCount;
