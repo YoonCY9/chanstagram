6de1adbb3d538645db419e9f1b283709acf096dc
YounTaeWoo <tty7797@naver.com>
2025-01-24 13:20:44 +0900

    delete : Query method 삭제

--- src/main/java/CTHH/chanstagram/post/postHashTag/PostHashTagRepository.java
+++ src/main/java/CTHH/chanstagram/post/postHashTag/PostHashTagRepository.java
@@ -7,10 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PostHashTagRepository extends JpaRepository<postHashTag, Long> {
+public interface PostHashTagRepository extends JpaRepository<PostHashTag, Long> {
 
-    // 특정 해시태그 ID와 연결된 모든 Post 반환
-    @Query("SELECT p.post FROM postHashTag p WHERE p.hashTag.id = :hashTagId")
-    List<Post> findPostsByHashTagId(@Param("hashTagId") Long hashTagId);
+
 }
 
