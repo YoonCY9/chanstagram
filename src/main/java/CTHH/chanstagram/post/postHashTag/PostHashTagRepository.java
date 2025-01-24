20f3c8c2a91d5ab6a5b38c9adb8b032008a6a980
YounTaeWoo <tty7797@naver.com>
2025-01-24 10:23:42 +0900

    fix : JPA QueryMethod 변경

--- src/main/java/CTHH/chanstagram/post/postHashTag/PostHashTagRepository.java
+++ src/main/java/CTHH/chanstagram/post/postHashTag/PostHashTagRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface PostHashTagRepository extends JpaRepository<postHashTag, Long> {
 
     // 특정 해시태그 ID와 연결된 모든 Post 반환
-    @Query("SELECT ph.post FROM Post_HashTag ph WHERE ph.hashTag.id = :hashTagId")
+    @Query("SELECT p.post FROM postHashTag p WHERE p.hashTag.id = :hashTagId")
     List<Post> findPostsByHashTagId(@Param("hashTagId") Long hashTagId);
 }
 
